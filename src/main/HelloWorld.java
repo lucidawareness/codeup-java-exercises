ade9b7f09116610eb16b8c9e0448e739075eaf9f
Irvin Ruiz <ruizirvin084@gmail.com>
2022-02-28 15:21:00 -0600

    finished Syntax, Types, and variables excercises

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -3,6 +3,37 @@ package main;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello, ");
-        System.out.print("World!");
+        System.out.println("World!");
+
+        int myFavoriteNumber = 0;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Irvin";
+        System.out.println(myString);
+
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+        x = 4;
+        x+= 5;
+
+        int y = 4;
+        y *= x;
+        System.out.println(y);
+
+        x = 10;
+        y = 2;
+        x /= y;
+        System.out.println(x);
+        y -= x;
+        System.out.println(y);
+
     }
 }
