ba1a4de827ed63c3a9e5a0b565d76135fdf7ece4
Irvin Ruiz <ruizirvin084@gmail.com>
2022-03-04 12:38:00 -0600

    finished input.java and input test.java exercises

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,7 +1,50 @@
 package util;
 
-public class Input {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Input {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String getString(String prompt) {
+        System.out.println(prompt);
+        return scanner.nextLine();
+    }
+
+    public static boolean yesNo(String prompt) {
+        System.out.println(prompt);
+        String userInput = scanner.nextLine();
+        return userInput.equalsIgnoreCase("y") || userInput.equalsIgnoreCase("yes");
+    }
+
+    public static int getInt(int min, int max, String prompt) {
+        System.out.println(prompt);
+        int userInput = scanner.nextInt();
+        if (userInput >= min && userInput <= max) {
+            return userInput;
+        } else {
+            System.out.printf("Please enter a number between %d and %d\n", min, max);
+            return getInt(min, max, prompt);
+        }
+    }
+
+    public static int getInt(String prompt) {
+        System.out.println(prompt);
+        return scanner.nextInt();
+    }
+
+    public static double getDouble(double min, double max, String prompt) {
+        System.out.println(prompt);
+        double userInput = scanner.nextDouble();
+        if (userInput >= min && userInput <= max) {
+            return userInput;
+        } else {
+            System.out.printf("Please enter a number between %d and %d\n", min, max);
+            return getDouble(min, max, prompt);
+        }
+    }
+
+    public static double getDouble(String prompt) {
+        System.out.println(prompt);
+        return scanner.nextDouble();
     }
 }
