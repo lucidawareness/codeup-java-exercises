04b8d0b6a70513522e13249db95e6f9d67d2a86f
Irvin Ruiz <ruizirvin084@gmail.com>
2022-03-03 19:49:37 -0600

    added ordinal numbers to SOUT asking user what number they will guess

--- src/main/HighLow.java
+++ src/main/HighLow.java
@@ -12,29 +12,32 @@ public class HighLow {
         int randomNum = ((int) (Math.random() * 100)) + 1;
         boolean correctGuess = false;
         int userGuessesLeft = 10;
-        System.out.println("We have generated a number between 1 and 100\nYou will have 10 attempts to guess the number and we will provide hints each time");
+        int userAttempts = 1;
+        System.out.println("Welcome to the guessing game.\nWe have generated a number between 1 and 100.\nYou will have 10 attempts to guess the number and we will provide hints each time.");
         do {
-            System.out.println("What is your guess?");
+            System.out.print(ordinalNumbers(userAttempts));
             int userGuess = scanner.nextInt();
             if (userGuess == randomNum) {
                 correctGuess = true;
                 System.out.printf("GOOD GUESS!\nYou have guesses correctly!\n The correct number was: %d \n", randomNum);
             } else {
                 userGuessesLeft -= 1;
+                userAttempts += 1;
                 if (userGuess < 1 || userGuess > 100) {
                     System.out.println("Please use a number between 1 and 100");
                 } else if (userGuess < randomNum && userGuessesLeft > 0) {
-                    System.out.println("Higher");
+                    System.out.println("\n--(Higher)--");
                 } else {
                     if (userGuessesLeft > 0) {
-                        System.out.println("LOWER");
+                        System.out.println("\n--(LOWER)--");
                     }
                 }
                 if (userGuessesLeft == 0) {
-                    System.out.println("GAME OVER!");
-                    System.out.printf("You have %d guesses left!\n", userGuessesLeft);
+                    System.out.println("\n\nGAME OVER!");
                     System.out.printf("The correct number was %d\n", randomNum);
                     correctGuess = true;
+                } else {
+                    System.out.printf("\nYou have %d guesses left!\n", userGuessesLeft);
                 }
             }
         } while (!correctGuess);
@@ -47,5 +50,17 @@ public class HighLow {
             System.out.println("Goodbye");
         }
         scanner.close();
+    }
+
+    public static String ordinalNumbers(int userAttempts){
+        if (userAttempts == 1) {
+            return "\nWhat is your " + userAttempts + "st guess?: ";
+        } else if (userAttempts == 2) {
+            return "\nWhat is your " + userAttempts + "nd guess?: ";
+        } else if (userAttempts == 3) {
+            return "\nWhat is your " + userAttempts + "rd guess?: ";
+        } else {
+            return "\nWhat is your " + userAttempts + "th guess?: ";
+        }
     }
 }
