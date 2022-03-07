b4f9158ef4f8c5c7bf9e739055ddd65d2975803d
Irvin Ruiz <ruizirvin084@gmail.com>
2022-03-07 11:46:55 -0600

    added and completed ArraysExercises, ServerNameGenerator

---
+++ src/ArraysExercises/ArraysExercises.java
@@ -0,0 +1,42 @@
+package ArraysExercises;
+
+import main.Person;
+
+import java.util.Arrays;
+
+public class ArraysExercises {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+//        To string needed due to array containing only int's.
+        System.out.println(Arrays.toString(numbers));
+
+        Person[] persons = new Person[3];
+
+        persons[0] = new Person("Irvin");
+        persons[1] = new Person("Julia");
+        persons[2] = new Person("Mike");
+
+        for (Person person : persons) {
+            System.out.println(person.getName());
+        }
+
+        System.out.println("New array");
+
+        Person person = new Person("Lilly");
+
+        Person[] persons2 = addPerson(persons, person);
+
+        for (Person person2 : persons2) {
+            System.out.println(person2.getName());
+        }
+
+
+    }
+
+
+    public static Person[] addPerson(Person[] persons, Person person) {
+        Person[] newPersons = Arrays.copyOf(persons, persons.length + 1);
+        newPersons[newPersons.length - 1] = person;
+        return newPersons;
+    }
+}
---
+++ src/ServerNameGenerator/ServerNameGenerator.java
@@ -0,0 +1,16 @@
+package ServerNameGenerator;
+
+public class ServerNameGenerator {
+    static String [] adjectives = {"Charming", "Cruel", "Fantastic", "Gentle", "Huge", "Perfect", "Rough", "Sharp"};
+    static String [] nouns = {"Tropicana", "Paris", "New York", "Maya", "Jupiter", "Google", "Oreo", "Ferrari"};
+
+    static String random(String[] arr){
+        return arr[(int) Math.floor(Math.random() *10)];
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Here is your server name: ");
+        System.out.printf("%s-%s", random(adjectives), random(nouns));
+    }
+
+}
